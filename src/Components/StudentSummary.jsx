98e048f99775f42cfc3f438ac1740ad349d40fb9
Vansh Sihag <vanshsihag001@gmail.com>
2026-04-22 23:15:40 +0530

    Update StudentSummary.jsx

--- src/Components/StudentSummary.jsx
+++ src/Components/StudentSummary.jsx
@@ -1,6 +1,6 @@
 
 
-function StudentsSummary() {
+function StudentsSummary({students}) {
   const total = students.length
 
     const passed = students.filter(student => student.score >= 40).length
@@ -19,21 +19,21 @@ function StudentsSummary() {
         <div className="flex flex-col border border-r-[#256594] flex-1 p-2 relative inline-block group cursor-pointer hover:bg-[#00FFE0]/10">
             <div className="flex flex-col">
             <span className="text-[#256594]">TOTAL</span>
-            <span className="text-[#00FFE0] text-5xl font-bold">4</span>
+            <span className="text-[#00FFE0] text-5xl font-bold">{total}</span>
             </div>
             <span className="absolute left-0 bottom-0 h-[2px] w-0 bg-[#00FFE0] transition-all duration-300 group-hover:w-full"></span>
         </div>
         <div className="flex flex-col border border-r-[#256594] flex-1 p-2 relative inline-block group cursor-pointer hover:bg-[#00FFE0]/10">
             <div className="flex flex-col">
                  <span className="text-[#256594]">PASSED</span>
-            <span className="text-[#00FFE0] text-5xl font-bold">4</span>
+            <span className="text-[#00FFE0] text-5xl font-bold">{passed}</span>
             </div>
             <span className="absolute left-0 bottom-0 h-[2px] w-0 bg-[#00FFE0] transition-all duration-300 group-hover:w-full"></span>
         </div>
         <div className="flex flex-col  flex-1 p-2 relative inline-block group cursor-pointer hover:bg-[#00FFE0]/10">
             <div className="flex flex-col">
                  <span className="text-[#256594]">AVG SCORE</span>
-            <span className="text-[#00FFE0] text-5xl font-bold">4</span>
+            <span className="text-[#00FFE0] text-5xl font-bold">AVG SCORE </span>
             </div>
               <span className="absolute left-0 bottom-0 h-[2px] w-0 bg-[#00FFE0] transition-all duration-300 group-hover:w-full"></span>
 
